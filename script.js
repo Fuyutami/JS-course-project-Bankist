75c0244aa7684e942cc951d63c1a11e52aa22d62
[Fuyutami] <fuyutami.art@gmail.com>
2021-08-05 21:32:28 +0300

    lesson 196: Lazy Loading Images lesson 197: Building a Slider Component: Part 1

--- script.js
+++ script.js
@@ -188,9 +188,77 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 })
 allSections.forEach(function (section) {
   sectionObserver.observe(section)
-  section.classList.add('section--hidden')
+  // section.classList.add('section--hidden')
 })
 
+/////////////////////////////////////////////////////////////////////////////////////
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]')
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries
+
+  if(!entry.isIntersecting) return
+
+  // Replace the src with data-src
+  entry.target.src = entry.target.dataset.src
+
+  entry.target.addEventListener('load', function name() {
+  entry.target.classList.remove('lazy-img')
+  })
+  observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img))
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Slider
+
+const slides = document.querySelectorAll('.slide')
+const btnLeft = document.querySelector('.slider__btn--left')
+const btnRight = document.querySelector('.slider__btn--right')
+
+let curSlide = 0
+const maxSlide = slides.length
+
+// const slider = document.querySelector('.slider')
+// slider.style.transform = 'scale(0.4) translateX(-800px)'
+// slider.style.overflow = 'visible'
+
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i-curSlide)}%)`)
+}
+
+goToSlide(0)
+
+// Next slide
+const nextSlide = function () {
+  if(curSlide === maxSlide - 1) {
+    curSlide = 0
+  } else {
+    curSlide++
+  }
+  goToSlide(curSlide)
+}
+
+const prevSlide = function () {
+  if(curSlide === 0) {
+    curSlide = maxSlide - 1
+  } else {
+    curSlide--
+  }
+  goToSlide(curSlide)
+}
+
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // PRACTICE SECTION ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
